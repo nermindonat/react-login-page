ef6f864357cd45cf744370173a9ab400939ea924
Nermin Donat <70170606+nermindonat@users.noreply.github.com>
2022-10-12 16:18:09 +0300

    RegisterForm.jsx component added.

--- src/components/registerForm/RegisterForm.jsx
+++ src/components/registerForm/RegisterForm.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import Button from "../button/Button";
-import "./loginForm.css";
-import { Link } from "react-router-dom";
+import React from 'react'
+import './registerForm.css'
 
-const LoginForm = () => {
+const RegisterForm = () => {
   return (
     <div className="login-form">
       <form>
+      <div className="first-name">
+          <label htmlFor="firstname">First Name</label>
+          <input type="text" placeholder="First Name" />
+        </div>
+        <div className="last-name">
+          <label htmlFor="lastname">Last Name</label>
+          <input type="text" placeholder="Last Name" />
+        </div>
         <div className="email">
           <label htmlFor="email">Email Address</label>
           <input type="text" placeholder="Email" />
@@ -17,11 +23,9 @@ const LoginForm = () => {
           </label>
           <input type="text" placeholder="Password" />
         </div>
-        <Button />
       </form>
-      <Link className='link' to="/register">Don't have an account?</Link>
     </div>
-  );
-};
+  )
+}
 
-export default LoginForm;
+export default RegisterForm
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,29 +1,11 @@
 import React from 'react'
+import RegisterForm from '../../components/registerForm/RegisterForm'
 
 const RegisterPage = () => {
   return (
-      <div className="login-form">
-      <form>
-      <div className="first-name">
-          <label htmlFor="firstname">First Name</label>
-          <input type="text" placeholder="First Name" />
-        </div>
-        <div className="last-name">
-          <label htmlFor="lastname">Last Name</label>
-          <input type="text" placeholder="Last Name" />
-        </div>
-        <div className="email">
-          <label htmlFor="email">Email Address</label>
-          <input type="text" placeholder="Email" />
-        </div>
-        <div className="password">
-          <label htmlFor="username" placeholder="Password">
-            Password
-          </label>
-          <input type="text" placeholder="Password" />
-        </div>
-      </form>
-    </div>
+      <div>
+        <RegisterForm />
+      </div>
   )
 }
 
